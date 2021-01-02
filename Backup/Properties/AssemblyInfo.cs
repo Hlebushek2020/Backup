d2c431358456b1e0d485e8729b36fca4812fbc17
Сергей <63193749+Hlebushek2020@users.noreply.github.com>
2021-01-02 18:20:06 +0300

    Update AssemblyInfo.cs

--- Backup/Properties/AssemblyInfo.cs
+++ Backup/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Backup")]
-[assembly: AssemblyCopyright("Sergey Govorunov 2019-2020")]
+[assembly: AssemblyCopyright("Sergey Govorunov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
